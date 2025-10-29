b24b0399749438f36b9ed22a5369df4fef29b2ed
Sean <mckinneysean91@gmail.com>
2025-10-29 10:10:51 +0100

    dodaje zadania z segmentu 4 python

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,13 @@
+###
+# Printing student's personal data
+#My name is Adam.
+#I am ... years old, and my height is ... cm.
+#In 6 years I will be ... years old.  
+#
+name = "Adam"
+height = 191
+age = 20
+age_in_6_years = age + 6
+print(f"My name is {name}.")
+print(f"I am {age} years old, and my height is {height} cm.")
+print(f"In 6 years I will be {age_in_6_years} years old.")
