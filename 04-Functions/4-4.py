2f4dd8f954574767b52e31648832615a2e7827e0
Sean <mckinneysean91@gmail.com>
2025-11-17 19:12:47 +0100

    dodaje coś tam z funkcji

--- 04-Functions/4-4.py
+++ 04-Functions/4-4.py
@@ -4,9 +4,12 @@
 def sum_digits(number):
     x = abs(number)
     x_string = str(x)
-    for char in 
-    return ...
-
+    
+    sum = 0
+    for char in x_string:
+        digit = int(char)
+        sum = sum + digit
+    return sum
 any_number = int(input('Enter integer number: '))
-result = sum_digits(...)
-print('The sum of the digits in the number ... is ...')
+result = sum_digits(any_number)
+print(f'The sum of the digits in the number {any_number} is', result)
